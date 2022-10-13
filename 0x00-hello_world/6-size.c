5c7473180ea69749ead04fc163ba62c90225663b
Thez <uba.thelmad@gmail.com>
2022-10-13 15:42:56 -0700

    Num 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,16 @@
 */
 int main(void)
 {
-	int a;
+	int a
+	char b
+	float c
+	long int d
+	long long int e;
 
-	printf("size of an int: %d bytes", sizeof(a));
+	printf("size of an int: %d byte(s)\n", sizeof(a))
+	printf("size of a char: %d byte(s)\n", sizeof(b))
+	printf("size of a float: %d bytes(s)\n", sizeof(c))
+	printf("size of a long int: %d byte(s)\n", sizeof(d))
+	ptintf("size of a long long int: %d byte(s)\n", sizeof(e));
 	return (0);
 }
