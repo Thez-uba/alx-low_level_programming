73ef9e0f1737a84950518a10c37a0f6d4dab5c12
Thez <uba.thelmad@gmail.com>
2022-10-13 14:12:13 -0700

    Num 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - prints Programming is like building a multilingual puzzle
+* main -prints "Programming is like building a multilingual puzzle
 * Return: void
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n")
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
