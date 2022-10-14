f1f726ceeccda80ea49161f9ca503f1466ccfd6c
Thez <uba.thelmad@gmail.com>
2022-10-14 12:35:32 -0700

    Num 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print if the number is positive, zero, or negative
+* Description: using main function
+* prints programmng is positive, zero, or negative
+* Return: void
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
