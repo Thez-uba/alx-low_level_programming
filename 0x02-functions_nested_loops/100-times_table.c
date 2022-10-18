37b1c61898f6bf066f1087a71bf172645915f841
Thez <uba.thelmad@gmail.com>
2022-10-18 11:15:01 -0700

    Num 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table starting with 0
+ * @n: The integer to times
+ */
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n < 15 || n > 0)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				else
+					_putchar((prod / 10) + '0');
+
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}	
