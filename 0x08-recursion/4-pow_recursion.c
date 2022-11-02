b5f70bad69145d081e52f257af5641425179ce2d
Thez <uba.thelmad@gmail.com>
2022-11-02 12:24:29 -0700

    Num

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  *  _pow_recursion - search a string for any of a set of byte
  *  @x: base
  *  @y: exposant
- *  Return: pointer to the byte in 's' that matche one of the bytes in 'accept'
+ *  Return: pointer to the byte in `s` that matche one of the bytes in 'accept'
  *  or NULL if no suuch byte is found.
  */
 
